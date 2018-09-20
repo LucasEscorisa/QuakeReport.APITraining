0c61fb88c28cdef2c72ef9288f00c87f87bb8db3
Lucas Escorisa <couto.lucas92@gmail.com>
2018-09-20 14:55:28 -0300

    - LoadManager implemented

--- app/src/main/java/com/example/android/quakereport/EarthquakeActivity.java
+++ app/src/main/java/com/example/android/quakereport/EarthquakeActivity.java
@@ -16,9 +16,11 @@
 package com.example.android.quakereport;
 
 import android.app.DownloadManager;
+import android.app.LoaderManager;
+import android.content.AsyncTaskLoader;
+import android.content.Context;
 import android.content.Intent;
 import android.net.Uri;
-import android.os.AsyncTask;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
@@ -26,11 +28,14 @@ import android.view.View;
 import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.ListView;
+import android.app.LoaderManager;
+import android.app.LoaderManager.LoaderCallbacks;
+import android.content.Loader;
 
 import java.util.ArrayList;
 import java.util.List;
 
-public class EarthquakeActivity extends AppCompatActivity {
+public class EarthquakeActivity extends AppCompatActivity implements LoaderCallbacks<List<Earthquake>>{
 
     public static final String LOG_TAG = EarthquakeActivity.class.getName();
 
@@ -38,14 +43,35 @@ public class EarthquakeActivity extends AppCompatActivity {
 
     private EarthquakeAdapter mAdapter;
 
+    private static final int EARTHQUAKE_LOADER_ID = 1;
+
+    @Override
+    public Loader<List<Earthquake>> onCreateLoader(int i, Bundle bundle){
+        return new EarthquakeLoader(this, USGS_REQUEST_URL);
+    }
+
+    @Override
+    public void onLoadFinished(Loader<List<Earthquake>> loader, List<Earthquake> earthquakes){
+        mAdapter.clear();
+
+        if(earthquakes != null && !earthquakes.isEmpty()){
+            mAdapter.addAll(earthquakes);
+        }
+    }
+
+    @Override
+    public void onLoaderReset(Loader<List<Earthquake>> loader){
+        mAdapter.clear();
+    }
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.earthquake_activity);
 
+        LoaderManager loaderManager = getLoaderManager();
 
-        EarthquakeAsyncTask task = new EarthquakeAsyncTask();
-        task.execute(USGS_REQUEST_URL);
+        loaderManager.initLoader(EARTHQUAKE_LOADER_ID, null, this);
 
 
         // Find a reference to the {@link ListView} in the layout
@@ -68,33 +94,10 @@ public class EarthquakeActivity extends AppCompatActivity {
                 startActivity(websintent);
             }
         });
+
+
     }
 
-    private class EarthquakeAsyncTask extends AsyncTask<String, Void, List<Earthquake>>{
-        @Override
-        protected List<Earthquake> doInBackground(String... urls){
 
-            if(urls.length < 1 || urls[0] == null){
-                return null;
-            }
 
-            List<Earthquake> result = QueryUtils.fetchEarthquakeData(urls[0]);
-
-            return result;
-        }
-
-        @Override
-        protected void onPostExecute(List<Earthquake> data){
-
-            mAdapter.clear();
-
-            if(data != null && !data.isEmpty()){
-                mAdapter.addAll(data);
-                Log.i(LOG_TAG, "ENTROU EM IF ---------------------" + data);
-
-            }
-            Log.i(LOG_TAG, "ENTROU EM DATA ---------------------" + data);
-
-        }
-    }
 }
